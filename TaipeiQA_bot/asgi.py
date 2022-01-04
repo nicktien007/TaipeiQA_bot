cd3d58645b23d49519dc305f998dd3c708620c61
nick <trojanhorse0077@gmail.com>
2022-01-05 00:28:17 +0800

    Initial commit

---
+++ TaipeiQA_bot/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TaipeiQA_bot project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaipeiQA_bot.settings')
+
+application = get_asgi_application()
