4be7951e9ca5694346d0ceacc1f3609c61155cb6
nick <trojanhorse0077@gmail.com>
2022-01-07 12:19:12 +0800

    adjust query

--- bot/views.py
+++ bot/views.py
@@ -23,8 +23,8 @@ headers = {"Authorization": settings.AUTH_TOKEN}
 
 def query(payload):
     retry_strategy = Retry(
-        total=3,
-        backoff_factor=1,
+        total=5,
+        backoff_factor=10,
         status_forcelist=[429, 500, 502, 503, 504],
         method_whitelist=["HEAD", "GET", "OPTIONS", "POST"]
     )
