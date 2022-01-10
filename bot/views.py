70860da51006e7bb89de1499ff5fc2193ca89f22
nick <trojanhorse0077@gmail.com>
2022-01-11 01:24:10 +0800

    adjust query

--- bot/views.py
+++ bot/views.py
@@ -22,29 +22,30 @@ headers = {"Authorization": settings.AUTH_TOKEN}
 
 
 def query(payload):
-    retry_strategy = Retry(
-        total=20,
-        backoff_factor=2,
-        status_forcelist=[429, 500, 502, 503, 504],
-        method_whitelist=["HEAD", "GET", "OPTIONS", "POST"]
-    )
-    adapter = HTTPAdapter(max_retries=retry_strategy)
+    # 不進行重試，heroku等待太久也丟出timeout的錯誤(錯誤碼：h12)
+    # retry_strategy = Retry(
+    #     total=20,
+    #     backoff_factor=2,
+    #     status_forcelist=[429, 500, 502, 503, 504],
+    #     method_whitelist=["HEAD", "GET", "OPTIONS", "POST"]
+    # )
+    # adapter = HTTPAdapter(max_retries=retry_strategy)
+    adapter = HTTPAdapter()
     http = requests.Session()
     http.mount("https://", adapter)
     http.mount("http://", adapter)
 
-    response = http.post(API_URL, headers=headers, json=payload,)
+    response = http.post(API_URL, headers=headers, json=payload)
 
     # response = requests.post(API_URL, headers=headers, json=payload, timeout=20)
-    print("query res:")
-    print(response)
+    # print("query res:")
+    # print(response)
     # print(response.json())
-    return response.json()
+    return response.status_code, response.json()
+
 
 @csrf_exempt
 def callback(request):
-
-
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
@@ -61,13 +62,17 @@ def callback(request):
             if isinstance(event, MessageEvent):
                 question = event.message.text
                 output = query({"inputs": question})
-                df = pd.DataFrame(output[0])
-                answer = df[df.score == df.score.max()].label.to_string(index=False)
-                replay_msg = f"你好😊\n關於您的提問，太Q已為您查詢到服務的局處💪💪\n歡迎聯繫「{answer}」，由專人來為您解答😀\n還有其他可以協助您的地方嗎？\n請將問題輸入對話框👇\n太Q將為您查詢服務的局處唷😃"
+
+                if output[0] != 200:
+                    replay_msg = f"您好😊\n歡迎使用Taipei QA 👋\nOhoh！小幫手太Q似乎睡著了呢💤\n請稍等20秒後再將問題輸入對話框👇\n太Q將立即為您查詢服務的局處唷😃"
+                else:
+                    df = pd.DataFrame(output[1])
+                    answer = df[df.score == df.score.max()].label.to_string(index=False)
+                    replay_msg = f"你好😊\n關於您的提問，太Q已為您查詢到服務的局處💪💪\n歡迎聯繫「{answer}」，由專人來為您解答😀\n還有其他可以協助您的地方嗎？\n請將問題輸入對話框👇\n太Q將為您查詢服務的局處唷😃"
+
                 line_bot_api.reply_message(
                     event.reply_token,
-                   TextSendMessage(text=replay_msg)
-                   # TextSendMessage(text = event.message.text)
+                    TextSendMessage(text=replay_msg)
                 )
         return HttpResponse()
     else:
